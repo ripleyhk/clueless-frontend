d2ad84cbd78483a849c411370641696da3279f49
Hannah Ripley <Hannah.Ripley@jhuapl.edu>
2024-03-26 09:17:49 -0400

    Added card and action panels

--- src/App.js
+++ src/App.js
@@ -1,82 +1,27 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import React from "react";
 
-import Board from './components/Board';
+import { Provider } from 'mobx-react';
+import { store } from "./stores/Stores";
+
+import Actions from "./components/Actions/Actions";
+import Board from './components/Board/Board';
+import Cards from "./components/Cards/Cards";
 
 import './App.css';
 
-const host = 'https://main--redactedclueless.netlify.app/.netlify/functions/server/';
-
-// const postMessage = async (user, message) => {
-//   await fetch(`${host}/messages`, {
-//   method: 'POST',
-//   body: JSON.stringify({
-//      content: message,
-//      username: user,
-//   }),
-//   headers: {
-//      'Content-type': 'application/json; charset=UTF-8',
-//   },
-//   })
-//   .then((response) => response.json())
-//   .catch((err) => {
-//      console.log(err.message);
-//   });
-//   };
-
-// const deleteMessage = async (id) => {
-//   await fetch(`${host}/messages/${id}`, {
-//      method: 'DELETE',
-//   })
-//   .then((response) => response.json())
-//   .catch((err) => {
-//      console.log(err.message);
-//   });
-//   };
-
 
 function App() {
-  const [username, setUsername] = useState('');
-  // const [message, setMessage] = useState('');
-  // const [messages, setMessages] = useState([]);
-  // const [count, setCount] = useState(0);
-
-
-  // const submitMessage = () => {
-  //   if (username.length > 0 && message.length > 0) {
-  //     console.log(`${username}: ${message}`);
-  //     postMessage(username, message);
-  //     setMessage('');
-  //   } else if (username.length === 0) {
-  //     alert("Please set a username.");
-  //   } else {
-  //     alert("Please write a message.")
-  //   }
-  // }
-
-  // const getMessages = () => {
-  //   axios.get(`${host}/messages`).then((data) => {
-  //     setMessages(data.data);
-  //     return data.data;
-  //   })
-  // }
-
-//   useEffect(getMessages, [count]);
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//         setCount(count + 1);
-//     }, 500);
-
-//     return () => clearInterval(interval);
-// }, [count]);
-
-
   return (
-    <div className="App">
+    <Provider store={store}>
+      <div className="App">
       <div className='content'>
-        <Board></Board>
+        <Cards/>
+        <Board/>
+        <Actions/>
       </div>
     </div>
-  );
+    </Provider>
+  )
 }
+
 export default App;
